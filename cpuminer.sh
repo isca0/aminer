52cc8112c0171e218c5087918d520c4baea5cf4b
Isca <igorsca@gmail.com>
2015-11-12 18:07:05 -0200

    ready for deploy

--- cpuminer.sh
+++ cpuminer.sh
@@ -6,82 +6,82 @@ read -p "senha " pass
 
 if [ $coin == "FCN+BCN" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://fcn-bcn.pool.minergate.com:45580 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://fcn-bcn.pool.minergate.com:45580 -u "$user" -p "$pass"
 fi
 
 if [ $coin == "FCN+XMR" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://fcn-mro.pool.minergate.com:45590 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://fcn-mro.pool.minergate.com:45590 -u "$user" -p "$pass"
 fi
 
 if [ $coin == "FCN+QCN" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://fcn-qcn.pool.minergate.com:45600 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://fcn-qcn.pool.minergate.com:45600 -u "$user" -p "$pass"
 fi
 
 if [ $coin == "FCN+XDN" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://fcn-duck.pool.minergate.com:45630 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://fcn-duck.pool.minergate.com:45630 -u "$user" -p "$pass"
 fi
 
 if [ $coin == "FCN+AEON" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://fcn-aeon.pool.minergate.com:45700 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://fcn-aeon.pool.minergate.com:45700 -u "$user" -p "$pass"
 fi
 
 if [ $coin == "FCN+DSH" ]
 then
 
-	echo "$comando" -a cryptonight -o stratum+tcp://fcn-dsh.pool.minergate.com:45730 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://fcn-dsh.pool.minergate.com:45730 -u "$user" -p "$pass"
 
 fi
 
 if [ $coin == "FCN+INF8" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://fcn-inf8.pool.minergate.com:45760 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://fcn-inf8.pool.minergate.com:45760 -u "$user" -p "$pass"
 
 fi
 
 if [ $coin == "MCN+BCN" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://mcn-bcn.pool.minergate.com:45650 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://mcn-bcn.pool.minergate.com:45650 -u "$user" -p "$pass"
 
 fi
 
 if [ $coin == "MCN+XMR" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://mcn-mro.pool.minergate.com:45660 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://mcn-mro.pool.minergate.com:45660 -u "$user" -p "$pass"
 
 fi
 
 if [ $coin == "MCN+QCN" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://mcn-qcn.pool.minergate.com:45670 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://mcn-qcn.pool.minergate.com:45670 -u "$user" -p "$pass"
 
 fi
 
 if [ $coin == "MCN+XDN" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://mcn-duck.pool.minergate.com:45680 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://mcn-duck.pool.minergate.com:45680 -u "$user" -p "$pass"
 
 fi
 
 if [ $coin == "MCN+AEON" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://mcn-aeon.pool.minergate.com:45710 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://mcn-aeon.pool.minergate.com:45710 -u "$user" -p "$pass"
 
 fi
 
 if [ $coin == "MCN+DSH" ]
 then
-	echo "$comando" -a cryptonight -o stratum+tcp://mcn-dsh.pool.minergate.com:45740 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://mcn-dsh.pool.minergate.com:45740 -u "$user" -p "$pass"
 
 fi
 
 if [ $coin == "MCN+INF8" ]
 then
 
-	echo "$comando" -a cryptonight -o stratum+tcp://mcn-inf8.pool.minergate.com:45770 -u "$user" -p "$pass"
+	${comando} -a cryptonight -o stratum+tcp://mcn-inf8.pool.minergate.com:45770 -u "$user" -p "$pass"
 
 fi
 exit 0
--- yam.sh
+++ yam.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
-read -p "yam? yam-core2? yam-ivy-bridge? " comando
+read -p "yam? yam-barcelona yam-bdver1 yam-bdver2 yam-core2? yam-haswell? yam-ivy-bridge? yam-nehalem? yam-sandy-bridge? " comando
 read -p "FCN+BCN FCN+XMR FCN+QCN MCN+BCN MCN+XMR MCN+QCN? " coin 
 read -p "usuario do yam? ex: fulano\%40gmail.com " user
 
 if [ $coin == "FCN+BCN" ]
 then
-	echo "$comando" -c x -M stratum+tcp://"$user":x@fcn-bcn.pool.minergate.com:45580/bcn
+	${comando} -c x -M stratum+tcp://"$user":x@fcn-bcn.pool.minergate.com:45580/bcn
 fi
 
 if [ $coin == "FCN+XMR" ]
 then
-	echo "$comando" -c x -M stratum+tcp://$user:x@fcn-mro.pool.minergate.com:45590/xmr
+	${comando} -c x -M stratum+tcp://$user:x@fcn-mro.pool.minergate.com:45590/xmr
 fi
 
 if [ $coin == "FCN+QCN" ]
 then
-	echo "$comando" -c x -M stratum+tcp://$user:x@fcn-qcn.pool.minergate.com:45600/qcn
+	${comando} -c x -M stratum+tcp://$user:x@fcn-qcn.pool.minergate.com:45600/qcn
 fi
 
 if [ $coin == "MCN+BCN" ]
 then
-	echo "$comando" -c x -M stratum+tcp://"$user":x@mcn-bcn.pool.minergate.com:45650/bcn
+	${comando} -c x -M stratum+tcp://"$user":x@mcn-bcn.pool.minergate.com:45650/bcn
 fi
 
 
 if [ $coin == "MCN+XMR" ]
 then
-	echo "$comando" -c x -M stratum+tcp://"$user":x@mcn-mro.pool.minergate.com:45660/xmr
+	${comando} -c x -M stratum+tcp://"$user":x@mcn-mro.pool.minergate.com:45660/xmr
 fi
 
 
 if [ $coin == "MCN+QCN" ]
 then
-	echo "$comando" -c x -M stratum+tcp://"$user":x@mcn-qcn.pool.minergate.com:45670/qcn
+	${comando} -c x -M stratum+tcp://"$user":x@mcn-qcn.pool.minergate.com:45670/qcn
 fi
 
 exit 0
